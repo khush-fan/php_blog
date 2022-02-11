07e599de4ec5c22571aff5da31635ec714cee925
Fanil <fanil.khush@yandex.ru>
2022-02-11 17:46:20 +0500

    add templates PostMapper

--- index.php
+++ index.php
@@ -1,88 +1,60 @@
-<!doctype html>
-<html lang="ru">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Blog\PostMapper;
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+require __DIR__ . '/vendor/autoload.php';
 
-    <title>Мой блог</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Блог</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Обо мне</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    <div class="container">
-        <h3 class="mb-2">Добро пожаловать в мой блог!</h3>
-        <div class="container-fluid row">            
-                <div class="card mr-2 p-1" style="width: 18rem;">
-                    <img src="https://dummyimage.com/300x200/263ccc/fff.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>           
-            
-                <div class="card mr-2 p-1" style="width: 18rem;">
-                    <img src="https://dummyimage.com/300x200/263ccc/fff.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>        
-           
-                <div class="card mr-2 p-1" style="width: 18rem;">
-                    <img src="https://dummyimage.com/300x200/263ccc/fff.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>  
-                <div class="card mr-2 p-1" style="width: 18rem;">
-                    <img src="https://dummyimage.com/300x200/263ccc/fff.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div class="card mr-2 p-1" style="width: 18rem;">
-                    <img src="https://dummyimage.com/300x200/263ccc/fff.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div> 
-                <div class="card mr-2 p-1" style="width: 18rem;">
-                    <img src="https://dummyimage.com/300x200/263ccc/fff.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>            
-        </div>
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
 
-    </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
-</html>
+$config = include 'config/database.php';
+$dsn = $config['dsn'];
+$username = $config['username'];
+$password = $config['password'];
+
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+} catch (PDOException $exception) {
+    echo 'Database error: '. $exception->getMessage();
+    die();
+}
+
+$postMapper = new PostMapper($connection);
+//Create app
+$app = AppFactory::create();
+
+
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view -> render('index.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view -> render('about.twig', [
+        'name' => 'Fanil'
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $post = $postMapper->getByUrlKey((string)$args['url_key']);
+    if (empty($post)) {
+        $body = $view->render('not-found.twig');
+    } else {
+    $body = $view->render('post.twig', [
+        'post' => $post
+    ]);
+    }
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
